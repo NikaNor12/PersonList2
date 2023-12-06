6f78cc7c5fc6087e1ce0fa49554be02affe2e22c
chich <chich@NIKA>
2023-12-06 17:29:23 +0400

    Add project files.

---
+++ G19_20231125/PersonList2.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+
+namespace G19_20231125
+{
+    public class PersonList2 : List<Person>, IList<Person>
+    {
+        private List<Person> _list;
+        private HashSet<int> _ids;
+
+        string[] _items = new string?[5];
+        public PersonList2()
+        {
+            _list = new List<Person>();
+            _ids = new HashSet<int>();
+        }
+
+        public void Add(Person item)
+        {
+            if (!_ids.Add(item.Id))
+            {
+                throw new ArgumentException("Id is not unique");
+            }
+
+            _list.Add(item);
+        }
+
+        public void Save(string filePath)
+        {
+            using (BinaryWriter writer = new(new FileStream(filePath, FileMode.Create)))
+            {
+                foreach (var person in this)
+                {
+                    writer.Write(person.Id);
+                    writer.Write(person.Firstname);
+                    writer.Write(person.Lastname);
+                    writer.Write(person.BirthDate.ToBinary());
+                    writer.Write((byte)person.Gender);
+                }
+            }
+        }
+
+        public new void Load(string filePath)
+        {
+            this.Clear();
+            this._ids.Clear();
+            using (BinaryReader reader = new(new FileStream(filePath, FileMode.Open)))
+            {
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    Person person = new Person
+                    {
+                        Id = reader.ReadInt32(),
+                        Firstname = reader.ReadString(),
+                        Lastname = reader.ReadString(),
+                        BirthDate = DateTime.FromBinary(reader.ReadInt64()),
+                        Gender = (GenderType)reader.ReadByte()
+                    };
+
+                    this.Add(person);
+                }
+            }
+        }
+
+        public Person this[int index]
+        {
+            get
+            {
+                return _list[index];
+            }
+            set
+            {
+                _list[index] = value;
+            }
+        }
+
+        public int Count => _list.Count;
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Clear()
+        {
+            _list = null;
+            _ids = null;
+        }
+
+        public bool Contains(Person item)
+        {
+            HashSet<int> ids = _ids;
+
+            while (ids.Contains(item.Id))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(Person[] array, int arrayIndex)
+        {
+            for (int i = 0; i < _list.Count; i++)
+            {
+                array.SetValue(_list[i], arrayIndex++);
+            }
+        }
+
+        public IEnumerator<Person> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        public int IndexOf(Person item) //mushaobs
+        {
+            return IndexOf(item, 0);
+        }
+        public int IndexOf(Person value, int startIndex) //mushaobs
+        {
+            for (int i = startIndex; i < _list.Count; i++)
+            {
+                if (_list[i].Equals(value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, Person item)
+        {
+            foreach (var person in this)
+            {
+                if (person.Id == item.Id)
+                {
+                    throw new ArgumentException("Id is not unique");
+                }
+            }
+
+            for (int i = _list.Count - 1; i > index; i--)
+            {
+                _list[i] = _list[i - 1];
+            }
+
+            _list[index] = item;
+        }
+
+        public void InsertRange(int index, Person[] items)
+        {
+            foreach (var item in items)
+            {
+                if (_ids.Contains(item.Id))
+                {
+                    throw new ArgumentException("Id is not unique");
+                }
+
+                _list.Insert(index++, item);
+            }
+        }
+
+        public bool Remove(Person item)
+        {
+            int index = IndexOf(item);
+
+            if (index != -1)
+            {
+                _list.RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < _list.Count - 1; i++)
+            {
+                _list[i] = _list[i + 1];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyListEnumerator(_items);
+        }
+        public class MyListEnumerator : IEnumerator
+        {
+            private object[] _items;
+            private int _index;
+            public MyListEnumerator(object[] items)
+            {
+                _items = items;
+                _index = -1;
+            }
+
+            public bool MoveNext()
+            {
+                if (_index + 1 < _items.Length)
+                {
+                    _index++;
+                    return true;
+                }
+                return false;
+            }
+            public void Reset()
+            {
+                _index = -1;
+            }
+            public object Current
+            {
+                get { return _items[_index]; }
+            }
+        }
+    }
+}
---
+++ G19_20231125/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace G19_20231125
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Gela.txt";
+
+            Person[] array = new Person[]
+            {
+                new Person {Id= 20, Firstname ="Zaza", Lastname="Beradze", BirthDate= new DateTime(1994, 3,4),Gender= GenderType.Male },
+                new Person {Id= 30,Firstname= "Shieda", Lastname ="kayn", BirthDate= new DateTime(2004, 4,4), Gender= GenderType.Male }
+            };
+
+            Person person1 = new Person
+            {
+                Id = 1,
+                Firstname = "Nika",
+                Lastname = "Chichua",
+                BirthDate = new DateTime(2004, 8, 4),
+                Gender = GenderType.Male
+            };
+            Person person2 = new Person(2, "Gulbaza", "Gulbazadze", new DateTime(1993, 3, 1), GenderType.Male);
+
+            Person person3 = new Person
+            {
+                Id = 3,
+                Firstname = "Gela",
+                Lastname = "Geladze",
+                BirthDate = new DateTime(2022, 1, 3),
+                Gender = GenderType.Male
+            };
+            Person person4 = new Person(4, "NikaJr", "Chichua", new DateTime(2030, 3, 1), GenderType.Male);
+
+            //person1.Children.Add(person4);
+
+            PersonList2 personList = new();
+            personList.Add(person1);
+            personList.Add(person2);
+            personList.Add(person3);
+
+            PersonList personList2 = new();
+            personList2.Add(person4);
+
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"{array[i]}");
+            //}
+            //personList[0]= person2;
+            //personList.Add(person4);          
+            //Console.WriteLine(personList.Contains(person4));
+            //personList.Insert(2, person3);
+
+            //personList.CopyTo(array, 0);
+            personList.InsertRange(3, array);
+
+
+            //for (int i = 0; i < personList.Count; i++)
+            //{
+            //    Console.WriteLine($"{personList[i]}");
+            //}
+
+
+            //personList.Clear();
+            //personList.Remove(person1);
+            //personList.RemoveAt(0);
+            //personList.InsertRange
+
+            //personList.Load(filePath);
+
+            personList.Save(filePath);
+            //personList.Load(filePath);
+            Console.WriteLine();
+           
+            foreach (var p in personList)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
